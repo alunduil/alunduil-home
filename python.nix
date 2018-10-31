2272ca64f9e50aced9cdfad8e28da9556a2c9908
Alex Brandt <alunduil@gmail.com>
2018-10-30 17:42:49 -0700

    Add Haskell language server configuration.

--- python.nix
+++ python.nix
@@ -2,6 +2,9 @@
 {
   home.packages = [
     pkgs.python3
+    pkgs.python36Packages.pyls-black
+    pkgs.python36Packages.pyls-isort
+    pkgs.python36Packages.pyls-mypy
     pkgs.python36Packages.python-language-server
     pkgs.python36Packages.virtualenv
   ];
